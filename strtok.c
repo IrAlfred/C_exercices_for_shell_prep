4cdf80f5705a2010d38da79972939e019796e119
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-19 02:01:27 -0700

    Replace strchr by my own _strchr

--- strtok.c
+++ strtok.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+/**
+ * _strchr - Search for the first occurence of a character
+ * @s: the string
+ * @c: the character
+ *
+ * Return: pointer to the first occurence found
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+	char *f;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+		{
+			break;
+		}
+		i++;
+	}
+	if (*(s + i) == c)
+	{
+		f = &s[i];
+	}
+	else
+	{
+		f = NULL;
+	}
+	return (f);
+}
+
 char *_strtok(char *string, const char *delim)
 {
 	static char *next_token;
@@ -22,7 +53,7 @@ char *_strtok(char *string, const char *delim)
 
 	while (*next_token)
 	{
-		if (strchr(delim, *next_token))
+		if (_strchr(delim, *next_token))
 		{
 			*next_token = '\0';
 			next_token++;
