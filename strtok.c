82d0ba47d2312db237f15daa2fdcf621d40bbe90
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-19 01:55:30 -0700

    Create my strtok

--- strtok.c
+++ strtok.c
@@ -3,21 +3,51 @@
 
 char *_strtok(char *string, const char *delim)
 {
-	int i = 0, j = 0;
-	bool found = false;
-
-	while (!found && string[i] != '\0')
+	static char *next_token;
+	char *current_token;
+	bool found;
+	if (string != NULL)
 	{
-		while (delim[j] != '\0')
-		{
-			if (string[i] == delim[
-			j++;
-		}
-		i++;
+		next_token = string;
 	}
 
+	/*Test if we are not at the end*/
+	if (next_token == NULL)
+	{
+		return (NULL);
+	}
+
+	current_token = next_token;
+	found = false;
+
+	while (*next_token)
+	{
+		if (strchr(delim, *next_token))
+		{
+			*next_token = '\0';
+			next_token++;
+			found = true;
+			break;
+		}
+		next_token++;
+	}
+
+	if (!found)
+	{
+		next_token = NULL;
+	}
+
+	return (current_token);
 }
 
 int main(void)
 {
+	char str[] = "Welcome to UAC university";
+	char *token = _strtok(str, ' ');
+	while (token != NULL)
+	{
+		printf("%s\n", token);
+		token = _strtok(NULL, ' ');
+	}
+	return (0);
 }
